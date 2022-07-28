b2081c743e9793fa1d802352ba04d05a620322b2
Salim Reza <82095538+salimreza12@users.noreply.github.com>
2022-07-28 14:03:34 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let result = document.getElementById("screen");
+
+let buttons = document.querySelectorAll("button");
+let screenValue = "";
+for (item of buttons) {
+  item.addEventListener("click", (e) => {
+    buttonText = e.target.innerText;
+    console.log(buttonText);
+
+    if (buttonText == "X") {
+      buttonText = "*";
+      screenValue += buttonText;
+      result.value = screenValue;
+    } else if (buttonText == "C") {
+      screenValue = "";
+      result.value = screenValue;
+    } else if (buttonText == "=") {
+      result.value = eval(screenValue);
+    } else if (buttonText == "DEL") {
+      result.value = screenValue.slice(-1);
+    } else {
+      screenValue += buttonText;
+      result.value = screenValue;
+    }
+  });
+}
